765d126bd49180b9edef87506c2a7dfb325ca40d
Pawel Jadach <pawel.jadach@empik.com>
2024-08-20 12:55:35 +0200

    minor config fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,11 +13,11 @@ export const config: Config = {
 			title: "Startup Computer",
 			description:
 				"Designing Future-Ready Applications for the Medical Sector",
-			metadataBase: new URL("https://pixel-blaze.com/"),
+			metadataBase: new URL("https://www.startup.computer/"),
 			openGraph: {
 				images: "/opengraph-image.png",
 			},
 		},
 	},
-	locale: "pl",
+	locale: "en",
 };
